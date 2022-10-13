7142161ac48758c4b9afc1b2ff634ad2aa4a0e5b
seanjung03 <seanjung03@gmail.com>
2022-10-13 15:49:38 -0400

    Add update functionality for image and name

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-fetch("https://pokeapi.co/api/v2/pokemon/1")
-  .then((data) => data.json())
-  .then((obj) => {
-    obj["moves"].forEach((move) => {
-      console.log(move["move"]["name"]);
-    });
-  });
-
 let index = 1;
 
 let down = document.querySelector(".down");
 let up = document.querySelector(".up");
 
-down.addEventListener("click" = () => {
+down.addEventListener("click", (e) => {
   index--;
+  if (index === 0) {
+    index = 898;
+  }
+  update();
   console.log(index);
 });
 
-up.addEventListener("click" = () => {
+up.addEventListener("click", (e) => {
   index++;
+  if (index === 899) {
+    index = 1;
+  }
+  update();
   console.log(index);
-})
-console.log("hello");
+});
+
+function update() {
+  let url = "https://pokeapi.co/api/v2/pokemon/" + index;
+  fetch(url)
+    .then((data) => data.json())
+    .then((obj) => {
+      /*obj["moves"].forEach((move) => {
+        console.log(move["move"]["name"]);
+      }); */
+
+      document.querySelector(".left img").src = obj["sprites"]["front_default"];
+      document.querySelector("h2").textContent = obj["species"]["name"];
+    });
+}
